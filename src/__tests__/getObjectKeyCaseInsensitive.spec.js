9340a55cb5ea353200827ab8ca3df1a8f669035a
Adam Finlayson <amf@adamfinlayson.com>
2021-11-05 21:19:51 -0500

    feat: getObjectKeyCaseInsensitive

---
+++ src/__tests__/getObjectKeyCaseInsensitive.spec.js
@@ -0,0 +1,51 @@
+const getObjectKeyCaseInsensitive = require("../getObjectKeyCaseInsensitive");
+
+//
+//
+// Mock constants
+//
+
+const TEST = {
+  VALUE: "testValue",
+};
+
+//
+//
+// Mock modules
+//
+
+//
+//
+// Mock environment
+//
+
+const DEFAULT_ENV = process.env;
+beforeEach(() => {
+  process.env = { ...process.env };
+});
+afterEach(() => {
+  process.env = DEFAULT_ENV;
+});
+
+//
+//
+// Run tests
+//
+
+describe("GetObjectKeyCaseInsensitive", () => {
+  it("Works", async () => {
+    const object = { one: TEST.VALUE };
+    const response = await getObjectKeyCaseInsensitive(object, "one");
+    expect(response).toBe(TEST.VALUE);
+  });
+  it("Matches lowercase to uppercase", async () => {
+    const object = { one: TEST.VALUE };
+    const response = await getObjectKeyCaseInsensitive(object, "ONE");
+    expect(response).toBe(TEST.VALUE);
+  });
+  it("Matches uppercase to lowercase", async () => {
+    const object = { ONE: TEST.VALUE };
+    const response = await getObjectKeyCaseInsensitive(object, "one");
+    expect(response).toBe(TEST.VALUE);
+  });
+});
