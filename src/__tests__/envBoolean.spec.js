56e75e57e8fcf801cdf9dba1f0fff8d69f60dc98
Adam Finlayson <amf@adamfinlayson.com>
2022-01-09 15:31:14 -0600

    test: remove async/await

--- src/__tests__/envBoolean.spec.js
+++ src/__tests__/envBoolean.spec.js
@@ -33,62 +33,62 @@ afterEach(() => {
 //
 
 describe("EnvBoolean", () => {
-  it("Returns undefined by default", async () => {
-    const response = await envBoolean(MOCK.KEY);
+  it("Returns undefined by default", () => {
+    const response = envBoolean(MOCK.KEY);
     expect(response).toBeUndefined();
   });
-  it("Returns a default value that can be set", async () => {
-    const response = await envBoolean(MOCK.KEY, { defaultValue: true });
+  it("Returns a default value that can be set", () => {
+    const response = envBoolean(MOCK.KEY, { defaultValue: true });
     expect(response).toBeTrue();
   });
-  it("Returns true when string true", async () => {
+  it("Returns true when string true", () => {
     process.env[MOCK.KEY] = "true";
-    const response = await envBoolean(MOCK.KEY);
+    const response = envBoolean(MOCK.KEY);
     expect(response).toBeTrue();
   });
-  it("Returns true when string TRUE", async () => {
+  it("Returns true when string TRUE", () => {
     process.env[MOCK.KEY] = "TRUE";
-    const response = await envBoolean(MOCK.KEY);
+    const response = envBoolean(MOCK.KEY);
     expect(response).toBeTrue();
   });
-  it("Returns true when boolean true", async () => {
+  it("Returns true when boolean true", () => {
     process.env[MOCK.KEY] = true;
-    const response = await envBoolean(MOCK.KEY);
+    const response = envBoolean(MOCK.KEY);
     expect(response).toBeTrue();
   });
-  it("Returns true when string 1", async () => {
+  it("Returns true when string 1", () => {
     process.env[MOCK.KEY] = "1";
-    const response = await envBoolean(MOCK.KEY);
+    const response = envBoolean(MOCK.KEY);
     expect(response).toBeTrue();
   });
-  it("Returns true when number 1", async () => {
+  it("Returns true when number 1", () => {
     process.env[MOCK.KEY] = 1;
-    const response = await envBoolean(MOCK.KEY);
+    const response = envBoolean(MOCK.KEY);
     expect(response).toBeTrue();
   });
-  it("Returns false when string false", async () => {
+  it("Returns false when string false", () => {
     process.env[MOCK.KEY] = "false";
-    const response = await envBoolean(MOCK.KEY);
+    const response = envBoolean(MOCK.KEY);
     expect(response).toBeFalse();
   });
-  it("Returns false when string FALSE", async () => {
+  it("Returns false when string FALSE", () => {
     process.env[MOCK.KEY] = "FALSE";
-    const response = await envBoolean(MOCK.KEY);
+    const response = envBoolean(MOCK.KEY);
     expect(response).toBeFalse();
   });
-  it("Returns false when boolean false", async () => {
+  it("Returns false when boolean false", () => {
     process.env[MOCK.KEY] = false;
-    const response = await envBoolean(MOCK.KEY);
+    const response = envBoolean(MOCK.KEY);
     expect(response).toBeFalse();
   });
-  it("Returns false when string 0", async () => {
+  it("Returns false when string 0", () => {
     process.env[MOCK.KEY] = "0";
-    const response = await envBoolean(MOCK.KEY);
+    const response = envBoolean(MOCK.KEY);
     expect(response).toBeFalse();
   });
-  it("Returns false when number 0", async () => {
+  it("Returns false when number 0", () => {
     process.env[MOCK.KEY] = 0;
-    const response = await envBoolean(MOCK.KEY);
+    const response = envBoolean(MOCK.KEY);
     expect(response).toBeFalse();
   });
 });
