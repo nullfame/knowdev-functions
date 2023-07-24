6dac55d36cf2bb9c7bc88abcd5ae594d2dfecfd5
Adam Finlayson <amf@adamfinlayson.com>
2023-07-24 13:00:43 -0500

    feat: placeholders

---
+++ src/__tests__/placeholders.spec.js
@@ -0,0 +1,29 @@
+const placeholders = require("../placeholders");
+
+//
+//
+// Run tests
+//
+
+describe("Placeholders", () => {
+  it("Works", () => {
+    const response = placeholders("Hello, {{name}}!", { name: "World" });
+    expect(response).toBeString();
+    expect(response).toBe("Hello, World!");
+  });
+  it("Works with two variables", () => {
+    const response = placeholders("{{greeting}}, {{name}}!", {
+      greeting: "Hi",
+      name: "there",
+    });
+    expect(response).toBeString();
+    expect(response).toBe("Hi, there!");
+  });
+  it("Does not replace unmapped variables", () => {
+    const response = placeholders("{{greeting}}, {{name}}!", {
+      greeting: "Hello",
+    });
+    expect(response).toBeString();
+    expect(response).toBe("Hello, {{name}}!");
+  });
+});
